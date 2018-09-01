fe5a7ca509b42df142151be238ac73f942cc2e46
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-01 19:12:03 +0300

    TDD for the vowels function tests for any kind of string and identifies duplicates and vowels in it

--- vowel_test.py
+++ vowel_test.py
@@ -5,6 +5,9 @@ class VowelTest (unittest.TestCase):
     
     def vowels_test(self):
         self.assertEqual(vowels("janet"), (['a','e'], 0))
+        self.assertEqual(vowels("urbanisation"), (['u', 'a', 'i', 'o'], 3))
+         self.assertEqual(vowels("ur,*sati!on"), (['u', 'a', 'i', 'o'], 0))
+
 
 if __name__== '__main__':
     unittest.main()
