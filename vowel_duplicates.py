f1289f251e37ec564e17bc63aa49e12b6740b243
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-01 19:09:42 +0300

    display vowels and duplicates in the string

--- vowel_duplicates.py
+++ vowel_duplicates.py
@@ -2,17 +2,17 @@
 def vowels(word):
     new_word = []
     vowel = ['a', 'e', 'i', 'o', 'u']
-    for x in word:
-        if x in vowel:
-           new_word.append(x)
-           count = new_word.count(x)
-           if count > 1:
-               new_word.pop(1)
-               count = count-1
-               return new_word, count
+    #get number of duplicates using set function
+    duplicates = len(word) - len(set(word))
 
-    return new_word
+    for x in word:
+        if x in vowel and x not in new_word:
+           new_word.append(x)
+        else:
+           continue
+
+    return new_word, duplicates
 
 #display new list
-word = "edahdah"
-print (vowels(word))
+print (vowels("edahdah"))
+print (vowels("drink water"))
