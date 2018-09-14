5c7870f0c56ad865324888826a4478d0105808d4
janetnamutebi460@gmail.com <janetnamutebi460@gmail.com>
2018-09-14 16:33:54 +0300

    only app files

---
+++ app/vowel_duplicates.py
@@ -0,0 +1,22 @@
+#function to count number of vowels in the string and return their duplicates
+def vowels(word):
+    #vowels set
+    vowel = set("aeiou")
+
+    #word without duplicates
+    word_set = set(word)
+
+    #get number of duplicates using set function
+    duplicates = len(word) - len(word_set)
+
+    #get the vowels in the word
+    new_word = word_set.intersection(vowel)
+    
+    #change new_word to a string
+    new_string_word ="".join(sorted(new_word))
+
+    return new_string_word, duplicates
+
+#display new list
+print (vowels("edahdah"))
+print (vowels("drink water"))
